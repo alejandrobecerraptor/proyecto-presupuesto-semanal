c6ccf02ae6b7479135a2c7048e103db0ad63d646
Alejandro <alejandro.becerra.pro@gmail.com>
2021-02-22 18:20:33 -0500

    Carga condicional de un componente, cargando un componente u otro

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
   //para el presupuesto.
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  //carga condicional de componentes
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
+
 
   return (
     <div className="container">
@@ -15,23 +18,29 @@ function App() {
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto = {guardarPresupuesto}
-            guardarRestante = {guardarRestante}
-          />
-          
-          <div className="row">
-            <div className="one-half column">
-              <Formulario
-              
+          { mostrarpregunta ? 
+            (
+              <Pregunta
+                  guardarPresupuesto = {guardarPresupuesto}
+                  guardarRestante = {guardarRestante}
+                  actualizarPregunta = {actualizarPregunta}
               />
+            ) 
+            : 
+            (
+              <div className="row">
+                <div className="one-half column">
+                  <Formulario
+                
+                  />
+                </div>
+                <div className="one-half column">
+                  2
+                </div>
             </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+            )
+          }  
         </div>
-
       </header>
     </div>
   );
